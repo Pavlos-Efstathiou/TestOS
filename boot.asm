cada7ffd5c4c047313fd45a4d2224ab13e7bb00c
Pavlos-Efstathiou <jimosmekma@gmail.com>
2021-03-07 18:12:10 +0200

    Added a logo on startup

--- boot.asm
+++ boot.asm
@@ -1,37 +1,30 @@
 bits 16
 
-org 0x7c00
+org 0x7c00 ; BIOS offset
 
 mov ah, 06h    ; Scroll up function
 xor al, al     ; Clear entire screen
 xor cx, cx     ; Upper left corner CH=row, CL=column
 mov dx, 184Fh  ; Lower right corner DH=row, DL=column 
-mov bh, 1Fh    ; black on white
+mov bh, 1Fh    ; Dark blue background, white letters
 int 10h
 
-mov si, text
-call print
-mov ah, 0
-int 16h
-jmp $ ; Infinite loop
+mov si, logo    ; Moves the logo variable to the si register to print it
+call print      ; calls print
+int 10h         ; Issue a BIOS interrupt for video services  
+jmp $           ; Infinite loop
 
 print:
-    ; Load byte at address si to al
-    lodsb
-    ; Check if al==0 / a NULL byte, meaning end of a C string
-    test al, al
-    ; If al==0, jump to end, where the bootloader will be halted
-    jz end
-    ; BIOS teletype
-    mov ah, 0Eh
-    ; Issue a BIOS interrupt 0x10 for video services
-    int 0x10                                                
-    ; Repeat
-    jmp print
-    end:
-        ret
+    lodsb           ; Load byte at address si to al
+    test al, al     ; Check if al==0 / a NULL byte, meaning end of a C string
+    jz end          ; Exits when a NULL byte is found  
+    mov ah, 0Eh     ; BIOS teletype
+    int 0x10        ; Issue a BIOS interrupt for video services                                                
+    jmp print       ; Repeat
+    end:            ; End label
+        ret         ; Returns the printed text
 
-text db "Bootloader loaded", 0Ah, 0Dh
-not_protected db "Not running under protected mode", 0Ah, 0Dh
+; The 0 ends the string. If I didn't put that there it would also print the next string
+logo db " _   _       _    __          ___           _                   ", 0Ah, 0Dh, "| \ | |     | |   \ \        / (_)         | |                  ", 0Ah, 0Dh, "|  \| | ___ | |_   \ \  /\  / / _ _ __   __| | _____      _____ ", 0Ah, 0Dh, "| . ` |/ _ \| __|   \ \/  \/ / | | '_ \ / _` |/ _ \ \ /\ / / __|", 0Ah, 0Dh, "| |\  | (_) | |_     \  /\  /  | | | | | (_| | (_) \ V  V /\__ \", 0Ah, 0Dh, "|_| \_|\___/ \__|     \/  \/   |_|_| |_|\__,_|\___/ \_/\_/ |___/", 0Ah, 0Dh, 0
 times 510 - ($ - $$) db 0
 dw 0xAA55
